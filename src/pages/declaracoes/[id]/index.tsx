e24cbfaa7e9df7bec5d956120aac88c8867f20ea
thfields <thiagoabcfc@hotmail.com>
2024-08-07 01:27:40 -0300

    feat: issue#28-adicionando-tipo-retificada
    issue#28

--- src/pages/declaracoes/[id]/index.tsx
+++ src/pages/declaracoes/[id]/index.tsx
@@ -75,6 +75,10 @@ export default function DeclaracaoPage() {
       </div>
       <div className="flex gap-10 text-lg mt-5">
         <span>
+          <span className="font-bold">Tipo: </span>
+          {data.retificacao ? "Retificada" : "Original"}
+        </span>
+        <span>
           <span className="font-bold">Data de envio: </span>
           {format(data.dataCriacao, "dd/MM/yyyy HH:mm")}
         </span>
