901cb24eeffaacfea45f1c4116f9d6f0a98c1336
thfields <thiagoabcfc@hotmail.com>
2025-01-13 23:50:19 -0300

    feat: implementa função de editar usuário
    issue:128

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -3,8 +3,19 @@ import Input from "../components/Input"
 import { Link } from "react-router-dom"
 import Table from "../components/Table"
 import { createColumnHelper, type ColumnDef } from "@tanstack/react-table"
-import { useSuspenseQuery } from "@tanstack/react-query"
+import { useSuspenseQuery, useMutation } from "@tanstack/react-query"
 import request from "../utils/request"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import toast from "react-hot-toast"
+
+// Definindo o esquema de validação
+const schema = z.object({
+  email: z.string().min(1, "Este campo é obrigatório").email("Email inválido"),
+  nome: z.string().min(1, "Este campo é obrigatório")
+})
+type FormData = z.infer<typeof schema>
 
 interface Museu {
   nome: string
@@ -44,6 +55,42 @@ const PerfilPage = () => {
     uf: museu.endereco.uf
   }))
 
+  // Configuração do React Hook Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onBlur",
+    defaultValues: {
+      email: user.email,
+      nome: user.nome
+    }
+  })
+
+  // Função para enviar os dados atualizados
+  const { mutate } = useMutation({
+    mutationFn: async ({ email, nome }: FormData) => {
+      const res = await request(`/api/admin/users/${user._id}`, {
+        method: "PUT",
+        data: { email, nome }
+      })
+      return res.json()
+    },
+    onSuccess: () => {
+      toast.success("Perfil atualizado com sucesso")
+      window.location.reload()
+    },
+    onError: () => {
+      toast.error("Erro ao atualizar perfil")
+    }
+  })
+
+  const onSubmit = ({ email, nome }: FormData) => {
+    mutate({ email, nome })
+  }
+
   return (
     <DefaultLayout>
       <Link to="/" className="text-lg">
@@ -52,22 +99,24 @@ const PerfilPage = () => {
       </Link>
       <h2>Perfil</h2>
       <div className="container mx-auto p-6 bg-white rounded-lg">
-        <form className="space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <div>
             <div className="grid grid-cols-3 gap-2 w-full">
               <Input
                 type="text"
                 label="Nome"
                 placeholder="Digite o nome"
+                error={errors.nome}
+                {...register("nome")}
                 className="w-full"
-                value={user.nome}
               />
               <Input
                 type="email"
                 label="Email"
                 placeholder="Digite o email"
+                error={errors.email}
+                {...register("email")}
                 className="w-full"
-                value={user.email}
               />
             </div>
           </div>
@@ -84,7 +133,10 @@ const PerfilPage = () => {
             <Link to="/" className="br-button secondary mt-5">
               Voltar
             </Link>
-            <button className="br-button primary mt-5" type="submit">
+            <button
+              className={`br-button primary mt-5 ${isSubmitting && "loading"}`}
+              type="submit"
+            >
               Salvar
             </button>
           </div>
