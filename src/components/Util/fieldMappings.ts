662389f2c92339c721c62fdc8d399348a01b9276
thfields <thiagoabcfc@hotmail.com>
2025-05-14 01:24:21 -0300

    feat: tela de listagem de pendencias

---
+++ src/components/Util/fieldMappings.ts
@@ -0,0 +1,59 @@
+export const museologicoFields = {
+  nderegistro: "Nº de Registro",
+  outrosnumeros: "Outros Números",
+  situacao: "Situação",
+  denominacao: "Denominação",
+  titulo: "Título",
+  autor: "Autor",
+  classificacao: "Classificação",
+  resumodescritivo: "Resumo Descritivo",
+  dimensoes: "Dimensões",
+  materialtecnica: "Material/Técnica",
+  estadodeconservacao: "Estado de Conservação",
+  localdeproducao: "Local de Produção",
+  datadeproducao: "Data de produção",
+  condicoesdereproducao: "Condições de Reprodução",
+  midiasrelacionadas: "Mídias Relacionadas"
+}
+
+export const bibliograficoFields = {
+  nderegistro: "Nº de Registro",
+  outrosnumeros: "Outros Números",
+  situacao: "Situação",
+  titulo: "Título",
+  tipo: "Tipo",
+  identificacaoderesponsabilidade: "Identificação da Responsabilidade",
+  localdeproducao: "Local de Produção",
+  editora: "Editora",
+  datadeproducao: "Data de Produção",
+  dimensaofisica: "Dimensão física",
+  materialtecnica: "Material/Técnica",
+  encadernacao: "Encadernação",
+  resumodescritivo: "Resumo Descritivo",
+  estadodeconservacao: "Estado de Conservação",
+  assuntoprincipal: "Assunto Principal",
+  assuntocronologico: "Assunto Cronológico",
+  assuntogeografico: "Assunto Geográfico",
+  condicoesdereproducao: "Condições de Reprodução",
+  midiasrelacionadas: "Mídias Relacionadas"
+}
+
+export const arquivisticoFields = {
+  coddereferencia: "Código de Referência",
+  titulo: "Título",
+  data: "Data",
+  niveldedescricao: "Nível de Descrição",
+  dimensaoesuporte: "Dimensão e Suporte",
+  nomedoprodutor: "Nome do Produtor",
+  historiaadministrativabiografia: "História Administrativa/Biografia",
+  historiaarquivistica: "História Arquivística",
+  procedencia: "Procedência",
+  ambitoeconteudo: "Âmbito e Conteúdo",
+  sistemadearranjo: "Sistema de Arranjo",
+  condicoesdereproducao: "Condições de Reprodução",
+  existenciaelocalizacaodosoriginais: "Existência e Localização dos Originais",
+  notassobreconservacao: "Notas sobre Conservação",
+  pontosdeacessoeindexacaodeassuntos:
+    "Pontos de Acesso e Indexação de Assuntos",
+  midiasrelacionadas: "Mídias Relacionadas"
+}
